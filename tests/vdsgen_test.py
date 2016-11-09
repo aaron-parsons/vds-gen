91cad31322b2cf13f37faeaf8c4a982552ed629c
Gary Yendell <gary.yendell@diamond.ac.uk>
2016-11-09 17:41:26 +0000

    First working version

--- tests/vdsgen_test.py
+++ tests/vdsgen_test.py
@@ -2,16 +2,168 @@ import unittest
 
 from pkg_resources import require
 require("mock")
-from mock import MagicMock, patch, ANY
+from mock import MagicMock, patch, ANY, call
+vdsgen_patch_path = "vdsgen.vdsgen"
+parser_patch_path = "argparse.ArgumentParser"
+h5py_patch_path = "h5py"
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "h5py"))
 
 from vdsgen import vdsgen
 
 
 class ParseArgsTest(unittest.TestCase):
 
-    @patch('argparse.ArgumentParser.parse_args')
-    def test_no_args_given(self, parse_mock):
-        vdsgen.parse_args()
+    @patch(parser_patch_path + '.add_argument')
+    @patch(parser_patch_path + '.parse_args')
+    def test_no_args_given(self, parse_mock, add_mock):
+        args = vdsgen.parse_args()
+
+        add_mock.has_calls(call("path", type=str,
+                                help="Path to folder containing HDF5 files."),
+                           call("prefix", type=str,
+                                help="Root name of images - e.g 'stripe_' to "
+                                     "combine the images 'stripe_1.hdf5', "
+                                     "'stripe_2.hdf5' and 'stripe_3.hdf5' "
+                                     "located at <path>."))
+        parse_mock.assert_called_once_with()
+        self.assertEqual(parse_mock.return_value, args)
+
+
+class FindFilesTest(unittest.TestCase):
+
+    @patch('os.listdir',
+           return_value=["stripe_1.h5", "stripe_2.h5", "stripe_3.h5",
+                         "stripe_4.h5", "stripe_5.h5", "stripe_6.h5"])
+    def test_given_files_then_return(self, _):
+        expected_files = ["/test/path/stripe_1.h5", "/test/path/stripe_2.h5",
+                          "/test/path/stripe_3.h5", "/test/path/stripe_4.h5",
+                          "/test/path/stripe_5.h5", "/test/path/stripe_6.h5"]
+
+        files = vdsgen.find_files("/test/path", "stripe_")
+
+        self.assertEqual(expected_files, files)
+
+    @patch('os.listdir', return_value=["stripe_1.h5"])
+    def test_given_1_files_then_error(self, _):
+
+        with self.assertRaises(IOError):
+            vdsgen.find_files("/test/path", "stripe_")
+
+    @patch('os.listdir', return_value=[])
+    def test_given_no_files_then_error(self, _):
+
+        with self.assertRaises(IOError):
+            vdsgen.find_files("/test/path", "stripe_")
+
+
+class SimpleFunctionsTest(unittest.TestCase):
+
+    def test_generate_vds_name(self):
+        expected_name = "stripe_vds.h5"
+        files = ["stripe_1.h5", "stripe_2.h5", "stripe_3.h5",
+                 "stripe_4.h5", "stripe_5.h5", "stripe_6.h5"]
+
+        vds_name = vdsgen.generate_vds_name("stripe_", files)
+
+        self.assertEqual(expected_name, vds_name)
+
+    mock_data = dict(data=MagicMock(shape=(3, 256, 2048), dtype="uint16"))
+
+    @patch(h5py_patch_path + '.File', return_value=mock_data)
+    def test_grab_metadata(self, h5file_mock):
+        expected_data = dict(frames=3, height=256, width=2048, dtype="uint16")
+
+        meta_data = vdsgen.grab_metadata("/test/path")
+
+        h5file_mock.assert_called_once_with("/test/path", "r")
+        self.assertEqual(expected_data, meta_data)
+
+    @patch(vdsgen_patch_path + '.grab_metadata',
+           return_value=dict(frames=3, height=256, width=2048, dtype="uint16"))
+    def test_process_source_datasets_given_valid_data(self, grab_mock):
+        files = ["stripe_1.h5", "stripe_2.h5"]
+        expected_source = vdsgen.Source(frames=3, height=256, width=2048,
+                                        dtype="uint16", datasets=files)
+
+        source = vdsgen.process_source_datasets(files)
+
+        grab_mock.assert_has_calls([call("stripe_1.h5"), call("stripe_2.h5")])
+        self.assertEqual(expected_source, source)
+
+    @patch(vdsgen_patch_path + '.grab_metadata',
+           side_effect=[dict(frames=3, height=256, width=2048, dtype="uint16"),
+                        dict(frames=4, height=256, width=2048,
+                             dtype="uint16")])
+    def test_process_source_datasets_given_mismatched_data(self, grab_mock):
+        files = ["stripe_1.h5", "stripe_2.h5"]
+
+        with self.assertRaises(ValueError):
+            vdsgen.process_source_datasets(files)
+
+        grab_mock.assert_has_calls([call("stripe_1.h5"), call("stripe_2.h5")])
+
+    def test_construct_vds_metadata(self):
+        source = vdsgen.Source(frames=3, height=256, width=2048,
+                               dtype="uint16", datasets=[""]*6)
+        expected_vds = vdsgen.VDS(shape=(3, 1586, 2048), offset=266,
+                                  path="/test/path")
+
+        vds = vdsgen.construct_vds_metadata(source, "/test/path")
+
+        self.assertEqual(expected_vds, vds)
+
+    @patch(h5py_patch_path + '.VirtualMap')
+    @patch(h5py_patch_path + '.VirtualSource')
+    @patch(h5py_patch_path + '.VirtualTarget')
+    def test_create_vds_maps(self, target_mock, source_mock, map_mock):
+        source = vdsgen.Source(frames=3, height=256, width=2048,
+                               dtype="uint16", datasets=["source"]*6)
+        vds = vdsgen.VDS(shape=(3, 1586, 2048), offset=266, path="/test/path")
+
+        map_list = vdsgen.create_vds_maps(source, vds)
+
+        target_mock.assert_called_once_with("/test/path", "full_frame",
+                                            shape=(3, 1586, 2048))
+        source_mock.assert_has_calls([call("source", "data",
+                                           shape=(3, 256, 2048))]*6)
+        # TODO: Improve this assert by passing numpy arrays to check slicing
+        map_mock.assert_has_calls([call(source_mock.return_value,
+                                        target_mock.return_value.__getitem__.return_value,
+                                        dtype="uint16")]*6)
+        self.assertEqual([map_mock.return_value]*6, map_list)
+
+
+class MainTest(unittest.TestCase):
+
+    file_mock = MagicMock()
+
+    @patch(h5py_patch_path + '.File', return_value=file_mock)
+    @patch(vdsgen_patch_path + '.create_vds_maps')
+    @patch(vdsgen_patch_path + '.construct_vds_metadata')
+    @patch(vdsgen_patch_path + '.process_source_datasets')
+    @patch(vdsgen_patch_path + '.generate_vds_name',
+           return_value="stripe_vds.h5")
+    @patch(vdsgen_patch_path + '.find_files')
+    @patch(vdsgen_patch_path + '.parse_args',
+           return_value=MagicMock(path="/test/path", prefix="stripe_"))
+    def test_correct_calls_made(self, parse_mock, find_mock, gen_mock,
+                                process_mock, construct_mock, create_mock,
+                                h5file_mock):
+
+        vdsgen.main()
 
         parse_mock.assert_called_once_with()
-
+        find_mock.assert_called_once_with("/test/path", "stripe_")
+        gen_mock.assert_called_once_with("stripe_", find_mock.return_value)
+        process_mock.assert_called_once_with(find_mock.return_value)
+        construct_mock.assert_called_once_with(process_mock.return_value,
+                                               "/test/path/stripe_vds.h5")
+        create_mock.assert_called_once_with(process_mock.return_value,
+                                            construct_mock.return_value)
+        h5file_mock.assert_called_once_with("/test/path/stripe_vds.h5", "w",
+                                            libver="latest")
+        self.file_mock.__enter__.return_value.create_virtual_dataset.assert_called_once_with(
+            VMlist=create_mock.return_value, fill_value=0x1)
--- vdsgen/vdsgen.py
+++ vdsgen/vdsgen.py
@@ -1,17 +1,188 @@
 #!/bin/env dls-python
 
+import os
 import sys
 from argparse import ArgumentParser
+import re
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+from collections import namedtuple
+Source = namedtuple("Source", "datasets, frames, height, width, dtype")
+VDS = namedtuple("VDS", "shape, offset, path")
+
+import h5py as h5
+
+DATA_SPACING = 10  # Pixel spacing between each sub image
 
 
 def parse_args():
+    """Parse command line arguments."""
     parser = ArgumentParser()
+    parser.add_argument("path", type=str,
+                        help="Path to folder containing HDF5 files.")
+    parser.add_argument("prefix", type=str,
+                        help="Root name of images - e.g 'stripe_' to combine "
+                             "the images 'stripe_1.hdf5', 'stripe_2.hdf5' "
+                             "and 'stripe_3.hdf5' located at <path>.")
+
     return parser.parse_args()
 
 
-def main():
+def find_files(path, prefix):
+    """Find HDF5 files in given folder with given prefix.
 
+    Args:
+        path(str): Path to folder containing files
+        prefix(str): Root name of image files
+
+    Returns:
+        list: HDF5 files in folder that have the given prefix
+
+    """
+    regex = re.compile(prefix + r"\d\.(hdf5|h5)")
+
+    files = []
+    for file_ in os.listdir(path):
+        if re.match(regex, file_):
+            files.append(os.path.join(path, file_))
+
+    if len(files) == 0:
+        raise IOError("No files matching pattern found.")
+    elif len(files) < 2:
+        raise IOError("Folder must contain more than one matching HDF5 file.")
+    else:
+        return files
+
+
+def generate_vds_name(prefix, files):
+    """Generate the file name for the VDS from the sub files.
+
+    Args:
+        prefix(str): Root name of image files
+        files: HDF5 being combined
+
+    Returns:
+        str: Name of VDS file
+
+    """
+    _, ext = os.path.splitext(files[0])
+    vds_name = "{prefix}vds{ext}".format(prefix=prefix, ext=ext)
+
+    return vds_name
+
+
+def grab_metadata(file_path):
+    """Grab data from given HDF5 file.
+
+    Args:
+        file_path(str): Path to HDF5 file
+
+    Returns:
+        Source: Number of frames, height and width of data, data type and
+            shape, (a tuple of (frames, height, width))
+
+    """
+    h5_data = h5.File(file_path, 'r')["data"]
+    frames, height, width = h5_data.shape
+    data_type = h5_data.dtype
+
+    return dict(frames=frames, height=height, width=width, dtype=data_type)
+
+
+def process_source_datasets(datasets):
+    """Grab data from the given HDF5 files and check for consistency.
+
+    Args:
+        datasets(list(str)): Datasets to grab data from
+
+    Returns:
+        int, Data: Number of files and attributes of data
+
+    """
+    data = grab_metadata(datasets[0])
+    for path in datasets[1:]:
+        temp_data = grab_metadata(path)
+        for attribute, value in data.items():
+            if temp_data[attribute] != value:
+                raise ValueError("Files have mismatched {}".format(attribute))
+
+    return Source(frames=data['frames'], height=data['height'],
+                  width=data['width'], dtype=data['dtype'], datasets=datasets)
+
+
+def construct_vds_metadata(source, output_file):
+    """Construct VDS data attributes from source attributes
+
+    Args:
+        source(Source): Attributes of data sets
+        output_file(str): File path of new VDS
+
+    Returns:
+        VDS: Shape and offset of virtual data set
+
+    """
+    datasets = len(source.datasets)
+    height = (source.height * datasets) + (DATA_SPACING * (datasets - 1))
+    shape = (source.frames, height, source.width)
+    offset = source.height + DATA_SPACING
+
+    return VDS(shape=shape, offset=offset, path=output_file)
+
+
+def create_vds_maps(source, vds_data):
+    """Create a list of VirtualMaps of raw data to the VDS.
+
+    Args:
+        source(Source): Source attributes
+        vds_data(VDS): VDS attributes
+
+    Returns:
+        list(VirtualMap): Maps describing links between raw data and VDS
+
+    """
+    source_shape = (source.frames, source.height, source.width)
+    vds = h5.VirtualTarget(vds_data.path, "full_frame", shape=vds_data.shape)
+
+    map_list = []
+    datasets = len(source.datasets)
+    for idx in range(datasets):
+        logging.info("Processing dataset %s", idx + 1)
+
+        source_file = source.datasets[idx]
+        v_source = h5.VirtualSource(source_file, "data", shape=source_shape)
+
+        start = idx * vds_data.offset
+        stop = start + source.height
+        v_target = vds[:, start:stop, :]
+
+        v_map = h5.VirtualMap(v_source, v_target, dtype=source.dtype)
+        map_list.append(v_map)
+
+    return map_list
+
+
+def main():
+    """Run program."""
     args = parse_args()
+
+    file_paths = find_files(args.path, args.prefix)
+    vds_name = generate_vds_name(args.prefix, file_paths)
+
+    file_names = [file_.split('/')[-1] for file_ in file_paths]
+    logging.debug("Combining datasets %s into %s",
+                  ", ".join(file_names), vds_name)
+
+    source = process_source_datasets(file_paths)
+    output_file = os.path.join(args.path, vds_name)
+    vds_data = construct_vds_metadata(source, output_file)
+
+    map_list = create_vds_maps(source, vds_data)
+
+    with h5.File(output_file, "w", libver="latest") as vds_file:
+        logging.info("Creating VDS at %s", output_file)
+        vds_file.create_virtual_dataset(VMlist=map_list, fill_value=0x1)
+        logging.info("Creation successful!")
 
 
 if __name__ == "__main__":
