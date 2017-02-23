9005cede517b9ffe4e83c9ab813df212bd6c27c6
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-02-23 09:51:37 +0000

    Add absolute import
    Also add from future import absolute_import

--- vdsgen/__init__.py
+++ vdsgen/__init__.py
@@ -1,4 +1,5 @@
 """Make 'generate_vds' easy to import."""
-from vdsgen import generate_vds
+from __future__ import absolute_import
+from vdsgen.vdsgen import generate_vds
 
 __all__ = ["generate_vds"]
