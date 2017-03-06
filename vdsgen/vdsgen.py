088099a87453dfafaa1e706f87073b234bde7f1c
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-03-06 11:27:13 +0000

    Update so data_path, stripe_spacing and module_spacing can be passed as arguments

--- vdsgen/vdsgen.py
+++ vdsgen/vdsgen.py
@@ -17,9 +17,9 @@ Source = namedtuple("Source",
                     ["datasets", "frames", "height", "width", "dtype"])
 VDS = namedtuple("VDS", ["shape", "spacing", "path"])
 
-INTER_MODULE_SPACING = 10  # Pixel spacing between stripes in a module
-MODULE_SPACING = 10        # Pixel spacing between modules
-DATA_PATH = "data"         # Location of data in HDF5 file tree
+STRIPE_SPACING = 10  # Pixel spacing between stripes in a module
+MODULE_SPACING = 10  # Pixel spacing between modules
+DATA_PATH = "data"   # Location of data in HDF5 file tree
 
 
 def parse_args():
@@ -31,6 +31,15 @@ def parse_args():
                         help="Root name of images - e.g 'stripe_' to combine "
                              "the images 'stripe_1.hdf5', 'stripe_2.hdf5' "
                              "and 'stripe_3.hdf5' located at <path>.")
+    parser.add_argument("-s", "--stripe_spacing", nargs="?", type=int,
+                        default=None, dest="stripe_spacing",
+                        help="Spacing between two stripes in a module.")
+    parser.add_argument("-m", "--module_spacing", nargs="?", type=int,
+                        default=None, dest="module_spacing",
+                        help="Spacing between two modules.")
+    parser.add_argument("-d", "--data_path", nargs="?", type=str, default=None,
+                        dest="data_path",
+                        help="Data location in HDF5 files.")
 
     return parser.parse_args()
 
@@ -79,7 +88,7 @@ def construct_vds_name(prefix, files):
     return vds_name
 
 
-def grab_metadata(file_path):
+def grab_metadata(file_path, data_path=None):
     """Grab data from given HDF5 file.
 
     Args:
@@ -118,24 +127,31 @@ def process_source_datasets(datasets):
                   width=data['width'], dtype=data['dtype'], datasets=datasets)
 
 
-def construct_vds_metadata(source, output_file):
+def construct_vds_metadata(source, output_file,
+                           stripe_spacing=None, module_spacing=None):
     """Construct VDS data attributes from source attributes.
 
     Args:
         source(Source): Attributes of data sets
         output_file(str): File path of new VDS
+        stripe_spacing(int): Spacing between stripes in module
+        module_spacing(int): Spacing between modules
 
     Returns:
         VDS: Shape, dataset spacing and output path of virtual data set
 
     """
-    stripes = len(source.datasets)
+    if stripe_spacing is None:
+        stripe_spacing = STRIPE_SPACING
+    if module_spacing is None:
+        module_spacing = MODULE_SPACING
 
+    stripes = len(source.datasets)
     spacing = [0] * stripes
     for idx in range(0, stripes - 1, 2):
-        spacing[idx] = INTER_MODULE_SPACING
+        spacing[idx] = stripe_spacing
     for idx in range(1, stripes, 2):
-        spacing[idx] = MODULE_SPACING
+        spacing[idx] = module_spacing
     spacing[-1] = 0  # We don't want the final stripe to have a gap afterwards
 
     height = (source.height * stripes) + sum(spacing)
@@ -144,12 +160,13 @@ def construct_vds_metadata(source, output_file):
     return VDS(shape=shape, spacing=spacing, path=output_file)
 
 
-def create_vds_maps(source, vds_data):
+def create_vds_maps(source, vds_data, data_path):
     """Create a list of VirtualMaps of raw data to the VDS.
 
     Args:
         source(Source): Source attributes
         vds_data(VDS): VDS attributes
+        data_path(str): Path to raw data in HDF5 file
 
     Returns:
         list(VirtualMap): Maps describing links between raw data and VDS
@@ -163,7 +180,7 @@ def create_vds_maps(source, vds_data):
     for idx, dataset in enumerate(source.datasets):
         logging.info("Processing dataset %s", idx + 1)
 
-        v_source = h5.VirtualSource(dataset, DATA_PATH, shape=source_shape)
+        v_source = h5.VirtualSource(dataset, data_path, shape=source_shape)
 
         start = current_position
         stop = start + source.height + vds_data.spacing[idx]
@@ -176,15 +193,21 @@ def create_vds_maps(source, vds_data):
     return map_list
 
 
-def generate_vds(path, prefix):
+def generate_vds(path, prefix, data_path, stripe_spacing, module_spacing):
     """Generate a virtual dataset.
 
     Args:
         path(str): Path to folder containing HDF5 files
         prefix(str): Prefix of HDF5 files to generate from (in <path> folder)
             e.g. image_ for image_1.hdf5, image_2.hdf5, image_3.hdf5
+        data_path(str): Path to raw data in HDF5 file
+        stripe_spacing(int): Spacing between stripes in module
+        module_spacing(int): Spacing between modules
 
     """
+    if data_path is None:
+        data_path = DATA_PATH
+
     file_paths = find_files(path, prefix)
     vds_name = construct_vds_name(prefix, file_paths)
     output_file = os.path.abspath(os.path.join(path, vds_name))
@@ -194,8 +217,9 @@ def generate_vds(path, prefix):
                  ", ".join(file_names), vds_name)
 
     source = process_source_datasets(file_paths)
-    vds_data = construct_vds_metadata(source, output_file)
-    map_list = create_vds_maps(source, vds_data)
+    vds_data = construct_vds_metadata(source, output_file,
+                                      stripe_spacing, module_spacing)
+    map_list = create_vds_maps(source, vds_data, data_path)
 
     logging.info("Creating VDS at %s", output_file)
     with h5.File(output_file, "w", libver="latest") as vds_file:
@@ -207,7 +231,9 @@ def generate_vds(path, prefix):
 def main():
     """Run program."""
     args = parse_args()
-    generate_vds(args.path, args.prefix)
+
+    generate_vds(args.path, args.prefix, args.data_path,
+                 args.stripe_spacing, args.module_spacing)
 
 if __name__ == "__main__":
     sys.exit(main())
